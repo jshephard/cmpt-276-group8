17f346820703a3f23dae3656c1fd7e7313473599
infinma <dta31@sfu.ca>
2015-10-18 00:39:21 +0000

    Changed Description to take in text

--- EventsHub/db/schema.rb
+++ EventsHub/db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016211443) do
+ActiveRecord::Schema.define(version: 20151016221319) do
 
   create_table "events", force: :cascade do |t|
     t.string   "Title"
-    t.string   "Description"
+    t.text     "Description"
     t.decimal  "Latitude",    precision: 6
     t.decimal  "Longitude",   precision: 6
     t.string   "Category"
