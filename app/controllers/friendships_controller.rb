4d0efe47109631d188680eb71b6de7f19878d03c
infinma <dta31@sfu.ca>
2015-11-24 22:10:27 +0000

    minor friend bug fixed

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -58,7 +58,7 @@ class FriendshipsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friendship
-      @friendship = Friendship.find(params[:id])
+      @friendship = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
